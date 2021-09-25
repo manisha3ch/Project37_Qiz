3fe64a9f1e5f4860bf10905308a8764049845f15
manisha3ch <73401128+manisha3ch@users.noreply.github.com>
2021-09-25 18:40:05 +0530

    Add files via upload

---
+++ js/Question.js
@@ -0,0 +1,84 @@
+class Question {
+
+  constructor() {
+    this.title = createElement('h1')
+    this.input1 = createInput("Enter Your Name Here....");
+    this.input2 = createInput("Enter Correct Option No..");
+    this.button = createButton('Submit');
+    this.button2 = createButton('Reset');
+    this.question = createElement('h3');
+    this.option1 = createElement('h4');
+    this.option2 = createElement('h4');
+    this.option3 = createElement('h4');
+    this.option4 = createElement('h4');
+  }
+
+  hide(){
+    this.title.hide();
+    this.input1.hide();
+    this.button.hide();
+    //this.button2.hide();
+    this.input2.hide();
+  }
+
+  display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(350, 0);
+
+    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.position(150, 80);
+    this.option1.html("1: Everyone " );
+    this.option1.position(150, 100);
+    this.option2.html("2: Envelope" );
+    this.option2.position(150, 120);
+    this.option3.html("3: Estimate " );
+    this.option3.position(150, 140);
+    this.option4.html("4: Example" );
+    this.option4.position(150, 160);
+
+    this.input1.position(150, 230);
+    this.input2.position(350, 230);
+    this.button.position(290, 300);
+    this.button2.position(360, 300);
+
+    this.button.mousePressed(()=>{
+      if(this.input1.value() != "" && this.input2.value() != 0){
+      this.title.hide();
+      this.input1.hide();
+      this.input2.hide();
+      this.button.hide();
+      contestant.name = this.input1.value();
+      contestant.answer = this.input2.value();
+      contestantCount+=1;
+      contestant.index = contestantCount;
+      contestant.update();
+      contestant.updateCount(contestantCount);
+      alert("Quiz Submitted");
+      }
+      else
+      alert("Try again");
+    });
+
+    this.button2.mousePressed(()=>{
+      contestant.updateCount(0)
+      quiz.update(0)
+      var ref = database.ref("contestants")
+      ref.remove()
+    })
+
+    this.input1.mouseClicked(()=>{
+      //alert("input1")
+      this.input1.value("");
+    })
+
+    this.input2.mousePressed(()=>{
+      //alert("input1")
+      if(this.input1.value() != "")
+      this.input1.attribute("disabled",true);
+
+      this.input2.attribute("type","number");
+      this.input2.attribute("min",0);
+      this.input2.attribute("max",4);
+    })
+  }
+}
